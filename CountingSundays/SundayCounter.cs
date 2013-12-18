5e3bc564b9f60c97075d466a371d66a345835a8a
Wayne Black <wblack@smchcn.net>
2013-12-18 09:01:47 -0600

    Initial commit

---
+++ CountingSundays/SundayCounter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingSundays
+{
+    public class SundayCounter
+    {
+        private readonly int[] _nonLeapYear = new[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        private readonly int[] _leapYear = new[] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        public bool IsSundayFirstOfMonth(int year, int month)
+        {
+            return GetSundaysAsFirstDayOfMonth(year, year).Contains(YearMonth(year,month));
+        }
+
+        public int GetMonthsWithFirstAsSunday(int startYear, int endYear)
+        {
+            return GetSundaysAsFirstDayOfMonth(startYear, endYear).Length;
+        }
+
+        private int[] GetSundaysAsFirstDayOfMonth(int startYear, int endYear)
+        {
+            const int yearIndex = 1900;
+            int dayIndex = 0;
+            var firstDayOfMonth = new List<int>();
+
+            for (int currentYear = yearIndex; currentYear < startYear; currentYear++)
+                dayIndex += AddYear(currentYear);
+
+            for (int currentYear = startYear; currentYear <= endYear; currentYear++)
+                for (int month = 1; month <= 12; month++)
+                {
+                    if (IsSunday(dayIndex))
+                        firstDayOfMonth.Add(YearMonth(currentYear,month));
+                    dayIndex += AddMonth(currentYear, month);
+                }
+
+            return firstDayOfMonth.ToArray();
+        }
+
+        private static int YearMonth(int currentYear, int month)
+        {
+            return currentYear * 100 + month;
+        }
+
+        private int AddYear(int year)
+        {
+            return IsLeapYear(year) ? _leapYear.Sum() : _nonLeapYear.Sum();
+        }
+
+        private int AddMonth(int year, int month)
+        {
+            return IsLeapYear(year) ? _leapYear[month - 1] : _nonLeapYear[month - 1];
+        }
+
+        private static bool IsLeapYear(int year)
+        {
+            return year%4 == 0 && year%400 != 0;
+        }
+
+        private static bool IsSunday(int day)
+        {
+            return day%7 == 0;
+        }
+    }
+}
---
+++ UnitTestCountSundays/SundayCounterTest.cs
@@ -0,0 +1,93 @@
+﻿using CountingSundays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCountSundays
+{
+    [TestClass]
+    public class SundayCounterTest
+    {
+        [TestMethod]
+        public void TestMethod_190101_FindFirstIsNotSunday()
+        {
+            TestIsNotFirstOfMonthSunday(1901, 1);
+        }
+        
+        [TestMethod]
+        public void TestMethod_190109_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1901, 9);
+        }
+        
+        [TestMethod]
+        public void TestMethod_190112_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1901, 12);
+        }
+        
+        [TestMethod]
+        public void TestMethod_190206_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1902, 6);
+        }
+
+        [TestMethod]
+        public void TestMethod_190302_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1903, 2);
+        }
+        
+        [TestMethod]
+        public void TestMethod_190303_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1903, 3);
+        }
+        
+        [TestMethod]
+        public void TestMethod_190311_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1903, 11);
+        }
+
+        [TestMethod]
+        public void TestMethod_190405_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1904, 5);
+        }
+
+        [TestMethod]
+        public void TestMethod_190501_FindFirstIsSunday()
+        {
+            TestIsFirstOfMonthSunday(1905, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod_1901_To_1905_NumberOfFirstIsSunday()
+        {
+            TestListOfSundays_Count(1901, 1905, 9);
+        }
+
+        [TestMethod]
+        public void TestMethod_1901_To_2000_NumberOfFirstIsSunday()
+        {
+            TestListOfSundays_Count(1901, 2000, 171);
+        }
+
+        private static void TestListOfSundays_Count(int startYear, int endYear, int expectedCount)
+        {
+            var sundays = new SundayCounter();
+            Assert.AreEqual(expectedCount, sundays.GetMonthsWithFirstAsSunday(startYear,endYear));
+        }
+
+        private static void TestIsFirstOfMonthSunday(int year, int month)
+        {
+            var sundays = new SundayCounter();
+            Assert.IsTrue(sundays.IsSundayFirstOfMonth(year, month));
+        }
+
+        private static void TestIsNotFirstOfMonthSunday(int year, int month)
+        {
+            var sundays = new SundayCounter();
+            Assert.IsFalse(sundays.IsSundayFirstOfMonth(year, month));
+        }
+    }
+}
